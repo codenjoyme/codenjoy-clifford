d8bf0b03dfa074dda4bf1b838da5524567030c4a
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-07 18:34:08 +0300

    [all][engine][events] Refactoring. Removed indexes from the verifyAllEvents.

--- src/test/java/com/codenjoy/dojo/clifford/game/AbstractGameCheckTest.java
+++ src/test/java/com/codenjoy/dojo/clifford/game/AbstractGameCheckTest.java
@@ -36,8 +36,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 
-import java.util.Arrays;
-
 public abstract class AbstractGameCheckTest extends AbstractGameTest {
 
     @Rule
@@ -90,16 +88,16 @@ public abstract class AbstractGameCheckTest extends AbstractGameTest {
             this.events = events;
         }
 
-        private String verifyAllEvents(Integer[] indexes) {
-            String actual = events.getEvents(indexes);
-            manager.appendCall(".verifyAllEvents", actual, Arrays.asList(indexes));
+        private String verifyAllEvents() {
+            String actual = events.getEvents();
+            manager.appendCall(".verifyAllEvents", actual);
             manager.end();
             return actual;
         }
 
         @Override
-        public void verifyAllEvents(String expected, Integer... indexes) {
-            String actual = verifyAllEvents(indexes);
+        public void verifyAllEvents(String expected) {
+            String actual = verifyAllEvents();
             if (callRealAssert) {
                 assertEquals(expected, actual);
             }
