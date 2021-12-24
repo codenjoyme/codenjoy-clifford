164d232a0e3ed0bc8b0b0b0dbaeacd524d3ac1db
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-12-24 16:32:47 +0200

    [clifford] Fixed bug after last commit.

--- src/test/java/com/codenjoy/dojo/clifford/game/check/AbstractGameCheckTest.java
+++ src/test/java/com/codenjoy/dojo/clifford/game/check/AbstractGameCheckTest.java
@@ -174,6 +174,7 @@ public abstract class AbstractGameCheckTest extends AbstractGameTest {
     @Override
     public GameSettings settings() {
         GameSettings result = manager.objectSpy(super.settings(),
+                "[-R]SettingsReader:initScore",
                 "[-R]SettingsReader:integer",
                 "[-R]SettingsReader:string",
                 "[-R]SettingsReader:bool");
